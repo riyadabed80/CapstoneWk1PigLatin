4bb32386c815f1291159e24ce695c85fcf163eeb
Riyad <rayabed@gmail.com>
2018-04-29 21:17:30 -0400

    First Commit

---
+++ CapstoneWeek1PigLatin/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace CapstoneWeek1PigLatin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool UserContinue = true;
+            while(UserContinue == true)
+            { 
+            Console.WriteLine("Please enter a word");
+
+                string UserInput = Console.ReadLine();
+
+                char [] vowels = { 'a', 'e', 'i', 'o', 'u'};
+
+                
+                int index = UserInput.ToLower().IndexOfAny(vowels);
+                
+                if (index==0)
+                {
+                    Console.WriteLine(UserInput + "way");
+                }
+                else if (index > 0)
+                {
+                    Console.WriteLine(UserInput.Substring(index) + 
+                        UserInput.Substring(0,index) + "ay");
+                }
+                else
+                {
+                    Console.WriteLine("Pig latin only works when the word has a vowel.");
+                }
+                    Console.WriteLine("Would you like to translate another word? y/n");
+                    string choice = Console.ReadLine();
+                if (choice.ToLower() == "no" || choice.ToLower() == "n")
+                { 
+                    UserContinue = false;
+                }
+
+
+            }
+            }
+    }
+}
